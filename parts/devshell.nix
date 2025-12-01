a46bcaa49f408f37b6b8235e1a7cafc7595adbd2
Chris Toph <toph@ryot.foo>
2025-11-30 19:00:09 -0500

    refactor: enhance development shell configuration by using lib.mkDefault

--- parts/devshell.nix
+++ parts/devshell.nix
@@ -1,27 +1,29 @@
 # Flake-parts module for development shell
 _: {
   perSystem =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
     {
       # Development shell for working on this library
-      devShells.default = pkgs.mkShell {
-        name = "lib-nix-dev";
+      devShells.default = lib.mkDefault (
+        pkgs.mkShell {
+          name = "lib-nix-dev";
 
-        packages = with pkgs; [
-          # Nix tooling
-          nil # Nix LSP
-          nixfmt-rfc-style # Official Nix formatter
-          statix # Nix linter
-          deadnix # Find dead code
+          packages = with pkgs; [
+            # Nix tooling
+            nil # Nix LSP
+            nixfmt-rfc-style # Official Nix formatter
+            statix # Nix linter
+            deadnix # Find dead code
 
-          # General dev tools
-          git
-        ];
+            # General dev tools
+            git
+          ];
 
-        shellHook = ''
-          echo "mix.nix development shell"
-        '';
-      };
+          shellHook = ''
+            echo "mix.nix development shell"
+          '';
+        }
+      );
 
       # Formatter for `nix fmt`
       formatter = pkgs.nixfmt-rfc-style;
